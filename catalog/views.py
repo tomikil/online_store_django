c7bce111ff1eb2b9adb4edd83108d49eabb17bfb
Tomilin Ivan <i.tomilin.91@yandex.ru>
2024-05-01 12:25:54 +0300

    added processing for collecting feedback with writing to a file

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        with open('catalog/data/contacts.txt', 'a', encoding="UTF-8") as file:
+            file.writelines(f"{name} ({phone}): {message}\n")
+        print(f"{name} ({phone}): {message}")
     return render(request, 'contacts.html')
