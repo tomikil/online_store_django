3fd3ad1c2d861982b3361d5291435450e9b28a3a
Tomilin Ivan <i.tomilin.91@yandex.ru>
2024-05-01 11:08:08 +0300

    settings Django project

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h&2k72^r&)k)ju2*v*b+&@)i%x()ofbb)ph_+t2^_c#5_xn=_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
